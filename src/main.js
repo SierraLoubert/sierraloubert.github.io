2d931c01c4bb8748c43721ff13f1a7c9f7820449
SierraLoubert <Loube1sl@cmich.edu>
2026-02-22 19:55:31 +0000

    Remove Load inspector

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ window.AFRAME = window.AFRAME || AFRAME;
 window.THREE = window.THREE || AFRAME.THREE;
 
 // Load inspector 
-const inspectorScript = document.createElement('script');
+// const inspectorScript = document.createElement('script');
 
-inspectorScript.src =
-  'https://unpkg.com/aframe-inspector@1.7.0/dist/aframe-inspector.min.js';
+// inspectorScript.src =
+//   'https://unpkg.com/aframe-inspector@1.7.0/dist/aframe-inspector.min.js';
 
-document.head.appendChild(inspectorScript);
+// document.head.appendChild(inspectorScript);
